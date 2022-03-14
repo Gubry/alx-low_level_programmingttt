b01f547bfcd0467d8b7acacd5e71ad0e5ca03cb4
Salami Ifeoluwa Mistura <omosholapesalami@gmial.com>
2022-03-14 06:19:42 -0700

    First code in C

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: using the main function
+ * This program prints a sentence
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
