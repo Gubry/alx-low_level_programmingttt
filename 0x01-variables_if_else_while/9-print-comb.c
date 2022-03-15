2fb5bf5fd9b471a47f2bac55d75f0b3665a320dc
Salami Ifeoluwa Mistura <omosholapesalami@gmial.com>
2022-03-15 04:56:29 -0700

    Combo

---
+++ 0x01-variables_if_else_while/9-print-comb.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c = 0;
+
+	while (48 + c)
+	{
+		putchar(c);
+		if (c != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
