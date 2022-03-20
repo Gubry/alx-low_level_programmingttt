68daa8640825198a4bcb21772dfa296d6cec80fd
Salami Ifeoluwa Mistura <omosholapesalami@gmial.com>
2022-03-20 10:02:12 -0700

    Addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - adds two numbers
+ * @a: one of the numbers to be added
+ * @b: the other number to perform the addition
+ *Return: the result of the addition
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
