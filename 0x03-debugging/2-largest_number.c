ae799345fa1496418929c0071025dbc4ba1cb006
Salami Ifeoluwa Mistura <omosholapesalami@gmial.com>
2022-03-21 04:39:56 -0700

    largest number

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * largest_number - returns the largest of three numbers
+ * @a:first number
+ * @b:second number
+ * @c:third number
+ * Return: largest number
+ */
+
+int largest_number(int a, int b, int c)
+{
+	int largest;
+
+	if (a > b && b > c)
+	{
+		largest = a;
+	}
+	else if (b > a && a > c)
+	{
+		largest = b;
+	}
+	else if (c > b)
+	{
+		largest = c;
+	}
+	else
+	{
+		largest = b;
+	}
+	return (largest);
+}
