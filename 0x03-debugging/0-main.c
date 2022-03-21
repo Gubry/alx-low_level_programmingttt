a611f03e3ffdf92c6aa16fd4e184979829b4d9ae
Salami Ifeoluwa Mistura <omosholapesalami@gmial.com>
2022-03-21 01:45:48 -0700

    Function Test

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - tests for negative or positive numbers
+ *@i: value to be checked
+ *Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
