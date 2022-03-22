07a0b6bed1b892617c5947145007106af075c092
Salami Ifeoluwa Mistura <omosholapesalami@gmial.com>
2022-03-22 05:47:29 -0700

    main

---
+++ 0x03-debugging/3-main_b.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - takes a day and print how many day are left in the year, taking
+ * leap year into consideration
+ * Return: 0
+ */
+
+int main(void)
+{
+	int month;
+	int day;
+	int year;
+
+	month = 2;
+	day = 29;
+	year = 2000;
+
+	printf("Date: %02d/%02d/%04d\n", month, day, year);
+
+	day = convert_day(day, month);
+
+	print_remaining_days(month, day, year);
+	return (0);
+}
