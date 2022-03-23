fe085dcf55da8bf6d170ec4f17d7a5986d3733fe
Salami Ifeoluwa Mistura <omosholapesalami@gmial.com>
2022-03-23 07:04:20 -0700

    multiplications

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ *@a: integer to multiply
+ *@b: integer to multiply
+ *Return: the result
+ */
+
+int mul(int a, int b)
+{
+	int z;
+
+	z = a * b;
+	return (z);
+}
