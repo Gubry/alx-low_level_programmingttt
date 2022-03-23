d2f1dd92fd9cfdee736fd6c09405f2b557da207e
Salami Ifeoluwa Mistura <omosholapesalami@gmial.com>
2022-03-23 04:53:08 -0700

    Bettystyled

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -9,9 +9,8 @@ int largest_number(int a, int b, int c);
 
 int convert_day(int month, int day);
 
-void print_remaining_days(int month, int day , int year);
+void print_remaining_days(int month, int day, int year);
 
-void print_times_table(int n);
 
 
 
