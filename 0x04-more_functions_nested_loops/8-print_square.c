38ac3a6d12115eb5418b420c25607158fa6aa1ca
Salami Ifeoluwa Mistura <omosholapesalami@gmail.com>
2022-03-26 03:29:55 -0700

    Change of name

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_square - print square shape
+ * @size: size of the square
+ * Return: the square
+ */
+
+void print_square(int size)
+{
+	int a, b;
+
+	if (size > 0)
+	{
+		for (a = 0; a < size; a++)
+		{
+			for (b = 0; b < size; b++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
